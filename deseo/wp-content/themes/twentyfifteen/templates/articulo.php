9fabef8c4f4ae46c7d82844006b7a52f95b3af71
gaburibe <gaburibe@gmail.com>
2016-10-18 11:31:06 -0500

    1

---
+++ deseo/wp-content/themes/twentyfifteen/templates/articulo.php
@@ -0,0 +1,231 @@
+<?php /* Template Name: articuloLD */ ?>
+
+	<!DOCTYPE html>
+
+<html class="nojs html css_verticalspacer" lang="en-US">
+
+ <!-- BC_OBNW -->
+<head>
+<title>ARCHER</title>
+<meta http-equiv="Content-type" content="text/html;charset=utf-8" />
+<meta name="image-hide-from-external-url" content="true" />
+<meta name="image-hide-location" content="true" />
+<meta name="generator" content="2015.2.1.352" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<script type="text/javascript">
+   // Update the 'nojs'/'js' class on the html node
+document.documentElement.className = document.documentElement.className.replace(/\bnojs\b/g, 'js');
+
+// Check that all required assets are uploaded and up-to-date
+if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "museconfig.js", "jquery.watch.js", "require.js", "archer.css"], "outOfDate":[]};
+</script>
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="css/site_global-crc=4162728713.css" />
+<link rel="stylesheet" type="text/css" href="css/archer-crc=278933197.css" id="pagesheet" />
+<!-- Other scripts -->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+  @font-face{font-family:tk-open-sans-n3;src:url(https://fonts.typekit.net/af/ed38c3/000000000000000000011c39/27/l?subset_id=1&fvd=n3) format("woff2");font-weight:300;font-style:normal;}@font-face{font-family:tk-open-sans-n4;src:url(https://fonts.typekit.net/af/4a9ae1/000000000000000000011c3b/27/l?subset_id=1&fvd=n4) format("woff2");font-weight:400;font-style:normal;}
+html { min-height: 100%; min-width: 100%; }
+body, div, dl, dt, dd, ul, ol, li, nav, h1, h2, h3, h4, h5, h6, pre, code, form, fieldset, legend, input, button, textarea, p, blockquote, th, td, a { margin: 0px; padding: 0px; border: 0px solid transparent; transform-origin: left top 0px; background-repeat: no-repeat; }
+table { border-collapse: collapse; border-spacing: 0px; }
+fieldset, img { border: 0px solid; transform-origin: left top 0px; }
+address, caption, cite, code, dfn, em, strong, th, var, optgroup { font-style: inherit; font-weight: inherit; }
+del, ins { text-decoration: none; }
+li { list-style: none; }
+caption, th { text-align: left; }
+h1, h2, h3, h4, h5, h6 { font-size: 100%; font-weight: inherit; }
+input, button, textarea, select, optgroup, option { font-family: inherit; font-size: inherit; font-style: inherit; font-weight: inherit; }
+input, textarea { -webkit-appearance: none; border-radius: 0px; }
+body { font-family: tk-open-sans-n3, "Helvetica Neue", Helvetica, sans-serif; text-align: left; font-size: 14px; line-height: 17px; word-wrap: break-word; text-rendering: optimizeLegibility; font-feature-settings: 'liga' 1; }
+a:link { color: rgb(0, 0, 255); text-decoration: inherit; }
+a:visited { color: rgb(128, 0, 128); text-decoration: inherit; }
+a:hover { color: rgb(0, 0, 255); text-decoration: inherit; }
+a:active { color: rgb(238, 0, 0); text-decoration: inherit; }
+a.nontext { color: black; text-decoration: none; font-style: normal; font-weight: normal; }
+.link { font-family: open-sans, sans-serif; font-size: 10px; font-weight: 400; padding: 0px; }
+.colelem { display: inline; float: left; clear: both; }
+.clearfix::after { content: " "; visibility: hidden; display: block; height: 0px; clear: both; }
+:first-child + html .clearfix { zoom: 1; }
+.clip_frame { overflow: hidden; }
+[data-whatinput="mouse"] :focus, [data-whatinput="touch"] :focus, input:focus, textarea:focus { outline: none; }
+textarea { resize: none; overflow: auto; }
+.MenuBar .SubMenuVisible, .MenuBarVertical .SubMenuVisible, .MenuBar .SubMenu .SubMenuVisible, .popup_element.Active, span.actAsPara, .actAsDiv, a.nonblock.nontext, img.block { display: block; }
+.widget_invisible, .js .invi, .js .mse_pre_init { visibility: hidden; }
+.borderbox { box-sizing: border-box; }
+.grpelem, .accordion_wrapper { display: inline; float: left; }
+.colelem100, .verticalspacer { clear: both; }
+.list0 li, .MenuBar .MenuItemContainer, .SlideShowContentPanel .fullscreen img, .css_verticalspacer .verticalspacer { position: relative; }
+.popup_element.Inactive, .js .disn, .js .an_invi, .hidden, .breakpoint { display: none; }
+#page { z-index: 1; min-height: 723.502px; border-width: 0px; border-color: rgb(0, 0, 0); padding-top: 21px; padding-bottom: 45.4979px; width: 100%; max-width: 959px; margin-left: auto; margin-right: auto; background-image: none; background-color: transparent; }
+#pu11513 { z-index: 2; width: 97.09%; margin-left: 1.99%; }
+#u11513 { z-index: 2; border-width: 0px; border-color: transparent; position: relative; margin-right: -10000px; width: 100%; background-color: transparent; }
+#u11520-4 { z-index: 3; min-height: 142px; color: rgb(214, 171, 123); text-align: center; font-size: 30px; line-height: 30px; font-family: open-sans, sans-serif; font-weight: 300; position: relative; margin-right: -10000px; margin-top: 35px; /*width: 34.38%; left: 54.68%;*/ background-color: transparent; }
+#u11520-2 { line-height: 0px; }
+#u11520 { font-size: 52px; line-height: 53px; }
+#u11526-11 { 
+	z-index: 7;
+    min-height: 327px;
+    color: rgb(127, 127, 127);
+    text-align: justify;
+    font-size: 16px;
+    line-height: 25px;
+    font-family: open-sans, sans-serif;
+    font-weight: 300;
+    position: relative;
+    /* margin-right: -10000px; */
+    margin-top: 122px;
+    /* width: 33.84%; */
+    /* left: 54.89%; */
+    background-color: transparent;
+ }
+#u11518 { z-index: 18; position: relative; margin-right: -10000px; width: 43.83%; left: 5.48%; background-color: transparent; }
+#u11518_img { width: 100%; }
+#u11521 { z-index: 20; border-width: 0px; border-color: transparent; position: relative; margin-right: -10000px; margin-top: 625px; width: 98.72%; left: 1.29%; background-color: transparent; }
+#u11523-4 { z-index: 21; min-height: 36px; color: rgb(127, 127, 127); text-align: center; font-size: 14px; font-family: open-sans, sans-serif; font-weight: 300; position: relative; margin-right: -10000px; margin-top: 11px; width: 100%; background-color: transparent; }
+#u11522 { z-index: 25; height: 38px; border: 1px solid rgb(82, 219, 170); position: relative; margin-right: -10000px; width: 11.76%; left: 28.62%; background-color: transparent; }
+#u11525 { z-index: 26; height: 38px; border: 1px solid rgb(82, 219, 170); position: relative; margin-right: -10000px; width: 11.76%; left: 44.29%; background-color: transparent; }
+#u11524 { z-index: 27; height: 38px; border: 1px solid rgb(82, 219, 170); position: relative; margin-right: -10000px; width: 11.76%; left: 59.85%; background-color: transparent; }
+#u11514 { z-index: 28; border-width: 0px; border-color: transparent; padding-bottom: 5px; position: relative; margin-right: -10000px; margin-top: 293px; width: 99.15%; background-color: transparent; }
+#u11516 { z-index: 30; height: 22px; border-color: rgb(251, 176, 59); opacity: 0.5; transform: matrix(0.7071, -0.7071, 0.7071, 0.7071, -5, 11); position: relative; margin-right: -10000px; margin-top: 5px; width: 2.39%; left: 0.55%; background-color: rgb(251, 176, 59); }
+#u11515 { z-index: 29; height: 22px; border-color: rgb(251, 176, 59); opacity: 0.41; transform: matrix(0.7071, -0.7071, 0.7071, 0.7071, -5, 11); position: relative; margin-right: -10000px; margin-top: 5px; width: 2.39%; left: 97.19%; background-color: rgb(82, 219, 170); }
+#u11517-4 { z-index: 31; min-height: 10px; opacity: 0.5; line-height: 14px; color: rgb(251, 176, 59); font-size: 12px; position: relative; margin-right: -10000px; margin-top: 505px; width: 1.29%; left: 13.22%; background-color: transparent; }
+#u12557-4 { z-index: 35; min-height: 23px; line-height: 10px; font-size: 8px; position: relative; margin-right: -10000px; margin-top: 433px; width: 8.17%; left: 90.77%; background-color: transparent; }
+#u13875-5 { z-index: 44; min-height: 21px; line-height: 12px; text-align: center; position: relative; margin-right: -10000px; margin-top: 141px; width: 9.13%; left: 89.69%; background-color: transparent; }
+#pu13116-4 { z-index: 39; margin-top: 22px; width: 11.37%; margin-left: 45.68%; }
+#u13116-4 { z-index: 39; min-height: 20px; color: rgb(127, 127, 127); line-height: 13px; text-align: center; text-transform: uppercase; font-size: 11px; font-family: open-sans, sans-serif; font-weight: 300; position: relative; margin-right: -10000px; margin-top: 8px; width: 100%; background-color: transparent; }
+#u13117 { z-index: 43; height: 29px; border: 1px solid rgb(82, 219, 170); margin-bottom: -1px; position: relative; margin-right: -10000px; width: 99.09%; background-color: transparent; }
+.css_verticalspacer .verticalspacer { height: calc(100vh - 790px); }
+#muse_css_mq, .html { background-color: rgb(255, 255, 255); }
+body { position: relative; min-width: 320px; }
+  .snippet{
+  	/*border:solid 1px #52DBAA;*/
+  	/*background-color: #f8f8f8;*/
+  	text-align: center;
+  	margin:7px;
+  	font-family: tk-open-sans-n3;
+  	color:rgb(214, 171, 123);
+  }
+</style>
+
+
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid" style="height: 65px !important;">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="http://www.desirelove.info/images/amor%20y%20deseo%20logo%20alta%20res.jpg?crc=4090970282" height="180%">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a style="colorr:#52DBAA;" href="#">Arquetipos <span class="sr-only">(current)</span></a></li>
+        <li ><a href="#">Conceptos <span class="sr-only">(current)</span></a></li>
+        <li ><a href="#">Animación <span class="sr-only">(current)</span></a></li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div class="container">
+		<div class="row">
+		    <div class="col-sm-4">
+		      <?
+		     	if ( has_post_thumbnail() ) {
+		     	
+				     the_post_thumbnail();
+				}
+		     ?>
+		    </div>
+		    <div class="col-sm-6">
+		      
+		      	 <div class="clearfix grpelem" id="u11520-4" data-muse-uid="U11520" data-muse-type="txt_frame"><!-- content -->
+			      <p id="u11520-2"><span id="u11520" style="font-family: tk-open-sans-n3"><?php the_title(  ); ?></span></p>
+			     </div>
+		       <div style="font-family: tk-open-sans-n3" class="clearfix grpelem" id="u11526-11" data-muse-uid="U11526" data-muse-type="txt_frame">
+		       <?php
+			    // TO SHOW THE PAGE CONTENTS
+
+			    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+			        <div class="entry-content-page">
+			            <?php the_content(); ?> <!-- Page Content -->
+			            <h1><?php the_field('descripcion'); ?></h1>
+			        </div><!-- .entry-content-page -->
+
+			    <?php
+			    endwhile; //resetting the page loop
+			    wp_reset_query(); //resetting the page query
+			    ?>
+			    </div>
+		    </div>
+		    <div class="col-sm-2">
+		      
+		       <?php
+
+			    while ( have_posts() ) : the_post(); ?>
+			        <div class="snippet">
+			           
+			            <a href="<?php the_field('redireccion'); ?>"><img style="max-width:80%" src="<?php the_field('galeria'); ?>"></a>
+			             <h1><?php the_field('descripcion'); ?></h1>
+			        </div>
+
+			    <?php
+			    endwhile; //resetting the page loop
+			    wp_reset_query(); //resetting the page query
+			    ?>
+		    </div>
+		     <div class="col-sm-2">
+		      
+		       <?php
+
+			    while ( have_posts() ) : the_post(); ?>
+			        <div class="snippet">
+			           
+			            <a href="<?php the_field('redireccion2'); ?>"><img style="max-width:80%" src="<?php the_field('galeria2'); ?>"></a>
+			             <h1><?php the_field('descripcion2'); ?></h1>
+			        </div>
+
+			    <?php
+			    endwhile; //resetting the page loop
+			    wp_reset_query(); //resetting the page query
+			    ?>
+		    </div>
+		    <div class="col-sm-2">
+		      
+		       <?php
+
+			    while ( have_posts() ) : the_post(); ?>
+			        <div class="snippet">
+			           
+			            <a href="<?php the_field('redireccion3'); ?>"><img style="max-width:80%" src="<?php the_field('galeria3'); ?>"></a>
+			             <h1><?php the_field('descripcion3'); ?></h1>
+			        </div>
+
+			    <?php
+			    endwhile; //resetting the page loop
+			    wp_reset_query(); //resetting the page query
+			    ?>
+		    </div>
+		    
+		</div>
+		<div class="row" style="font-family: tk-open-sans-n3">
+		   
+		    
+		   
+		    
+		</div>
+	</div>
+ 
+
+ 
+
+ 
+
+   
+</body>
+</html>
+<!-- Localized -->
+
+
---
+++ deseo/wp-content/themes/twentyfifteen/templates/homeesp.php
@@ -0,0 +1,127 @@
+<?php /* Template Name: articuloLD */ ?>
+
+	<!DOCTYPE html>
+
+<html class="nojs html css_verticalspacer" lang="en-US">
+
+ <!-- BC_OBNW -->
+<head>
+<title>ARCHER</title>
+<meta http-equiv="Content-type" content="text/html;charset=utf-8" />
+<meta name="image-hide-from-external-url" content="true" />
+<meta name="image-hide-location" content="true" />
+<meta name="generator" content="2015.2.1.352" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<script type="text/javascript">
+   // Update the 'nojs'/'js' class on the html node
+document.documentElement.className = document.documentElement.className.replace(/\bnojs\b/g, 'js');
+
+// Check that all required assets are uploaded and up-to-date
+if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "museconfig.js", "jquery.watch.js", "require.js", "archer.css"], "outOfDate":[]};
+</script>
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="css/site_global-crc=4162728713.css" />
+<link rel="stylesheet" type="text/css" href="css/archer-crc=278933197.css" id="pagesheet" />
+<!-- Other scripts -->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+
+body{
+	background:url("<?php echo get_stylesheet_directory_uri(); ?>/images/circles.jpg");
+	background-size: cover;
+}
+.squarees{
+	position:absolute;
+	bottom:150px;
+	width:200px;
+	height: 50px;
+	border: solid 1px #a5cec0;
+	left: 0; 
+  right: 0; 
+  margin-left: auto; 
+  margin-right: auto; 
+  text-align: center;
+  font-size: 20px;
+  color:#a5cec0;
+  padding-top: 10px;
+  background-color: white;
+}
+.squareen{
+	position:absolute;
+	bottom:80px;
+	width:200px;
+	height: 50px;
+	border: solid 1px #a5cec0;
+	left: 0; 
+  right: 0; 
+  margin-left: auto; 
+  margin-right: auto; 
+  text-align: center;
+  font-size: 20px;
+  color:#a5cec0;
+  padding-top: 10px;
+  background-color: white;
+}
+.squarees:hover{
+	background-color: #a5cec0;
+	color:white;
+	cursor: pointer;
+}
+.squareen:hover{
+	background-color: #a5cec0;
+	color:white;
+	cursor: pointer;
+}
+</style>
+
+
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid" style="height: 65px !important;">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="http://www.desirelove.info/images/amor%20y%20deseo%20logo%20alta%20res.jpg?crc=4090970282" height="180%">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a style="colorr:#52DBAA;" href="#">Arquetipos <span class="sr-only">(current)</span></a></li>
+        <li ><a href="#">Conceptos <span class="sr-only">(current)</span></a></li>
+        <li ><a href="#">Animación <span class="sr-only">(current)</span></a></li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div class="container">
+		<div class="row">
+		    <div class="col-sm-12" style="text-align: center;">
+		      
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/amordeseo.jpg">
+						<br>
+						
+						
+			    
+		    </div>
+
+		   
+		    
+		</div>
+		
+	</div>
+	<div class="squarees " >Español</div><Br>
+		      	<div class="squareen ">English</div>
+ 
+
+ 
+
+ 
+
+   
+</body>
+</html>
+<!-- Localized -->
+
+
